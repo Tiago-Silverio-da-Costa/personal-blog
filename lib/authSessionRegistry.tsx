4c93011d1c3df2a1b38401bb0ff37ff4c7269d58
Tiago-Silverio-da-Costa <tiagosilveriodacosta@gmail.com>
2024-06-05 10:01:46 -0300

    basePath in sessionProvider

--- lib/authSessionRegistry.tsx
+++ lib/authSessionRegistry.tsx
@@ -7,5 +7,5 @@ export default function AuthSessionProvider({
 }: {
   children: React.ReactNode;
 }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider basePath="/api/auth">{children}</SessionProvider>;
 }
